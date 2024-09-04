97575022c5be9cd8f3129c82c5a6bedbb05bbea8
kimmingyu0625 <tkwls003@naver.com>
2024-09-04 15:25:15 +0900

    Revert "Fix. react + springboot security 작업 임시저장"
    This reverts commit 6a2d768a88ae4337a74c66549ce6fc412e1b9b6d.

--- photo/src/main/java/photo_app_project/photo/constants/SecurityConstants.java
+++
@@ -1,9 +0,0 @@
-package photo_app_project.photo.constants;
-
-public class SecurityConstants {
-
-    public static final String TOKEN_HEADER = "Authorization";
-    public static final String TOKEN_PREFIX = "Bearer ";
-    public static final String TOKEN_TYPE = "JWT";
-
-}
